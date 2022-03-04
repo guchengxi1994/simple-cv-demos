1b8762a89337b6d5ee72268d696ea6071f301a4a
guchengxi1994 <guchengxi1994@qq.com>
2022-03-04 16:20:33 +0800

    Update __init__.py

--- decorators/__init__.py
+++ decorators/__init__.py
@@ -1,10 +1,34 @@
 class CvToolDecorator:
     @staticmethod
-    def checkParam(paramName, paramTypeList: list = [], paramRange: list = []):
+    def checkParam(paramName:str = "",
+                   paramIndex: int = -1,
+                   paramTypeList: list = [],
+                   paramRange: list = []):
+        """using parameter's name is recommended
+
+           Params
+           -------
+           paramName : the parameter name in **kwargs
+           paramIndex : optional, the parameter index in *args
+           paramTypeList : parameter types ,such as [int,str,tuple,...]
+           paramRange : parameter values ,such as ["I","love","China"]
+
+        """
         def wrapper(func):
             def inner(*args, **kwargs):
                 _arg = kwargs.get(paramName, None)
                 if _arg is not None:
+                    if len(paramTypeList) > 0:
+                        assert type(
+                            _arg
+                        ) in paramTypeList, "input error, should be a {}".format(
+                            " or ".join(list(str(x) for x in paramTypeList)))
+
+                    if len(paramRange) > 0:
+                        assert _arg in paramTypeList, "input error"
+
+                if paramIndex != -1 and paramIndex < len(args):
+                    _arg = args[paramIndex]
                     if len(paramTypeList) > 0:
                         assert type(
                             _arg
